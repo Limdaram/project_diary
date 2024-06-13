07ff8a2039f8be61be91c19f979b662a81a18cdc
Limdaram <clara9182@naver.com>
2024-06-13 23:16:17 +0900

    login background

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,6 @@
 import {Box, Button, Stack, TextField, Typography} from "@mui/material";
 import {useNavigate} from "react-router-dom";
+import loginImg from '../../img/loginImg.jpg';
 
 
 // 위에 메뉴들 그리기
@@ -9,20 +10,41 @@ export default function Login() {
         navigate('/signup');
     }
     return (
-        <>
-            <Box
-                sx={{ height: 500, width: 500, border: '2px solid grey', alignItems:'center'}}>
-                <Stack>로그인</Stack>
-                <Typography>
-                    아이디 <TextField  sx={{ml:2}} id="outlined-basic" label="ID" variant="outlined" />
-                </Typography>
-                <Typography>
-                    비밀번호 <TextField sx={{ml:2}} id="outlined-basic" label="PW" variant="outlined" />
-                </Typography>
-                <Button color="secondary" onClick={() => onClickSignUp()}>회원가입</Button>
-            </Box>
+        <div style={{
+            backgroundImage: `url(${loginImg})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'top center',
+            backgroundAttachment: 'fixed',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }}>
+            <Stack
+                sx={{
+                    padding: 3,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                }}
+            >
+                <Stack spacing={2} alignItems="center">
+                    <Typography variant="h4" color='black'>로그인</Typography>
+                    <Box sx={{display: 'flex', alignItems: 'center'}}>
+                        <Typography color='black'>아이디</Typography>
+                        <TextField sx={{ml: 2}} id="outlined-basic" label="ID" variant="outlined"/>
+                    </Box>
+                    <Box sx={{display: 'flex', alignItems: 'center'}}>
+                        <Typography color='black' >비밀번호</Typography>
+                        <TextField sx={{ml: 2}} id="outlined-basic" label="PW" variant="outlined"/>
+                    </Box>
+                </Stack>
+                <Stack direction='row' justifyContent='flex-end' mt={5}>
+                    <Button color="secondary" onClick={onClickSignUp}>회원가입</Button>
+                </Stack>
+            </Stack>
+        </div>
 
-        </>
-
-    )
-}
+        )
+    }
